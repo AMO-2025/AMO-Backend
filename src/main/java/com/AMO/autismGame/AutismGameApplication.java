a5c667a49849450cc8fa545aaf34348684e0d0f6
seongwon Choi <flqjglf33@gmail.com>
2025-02-28 16:17:57 +0900

    chore(all)finish backend setting

---
+++ src/main/java/com/AMO/autismGame/AutismGameApplication.java
@@ -0,0 +1,12 @@
+package com.AMO.autismGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutismGameApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ src/test/java/com/AMO/autismGame/AutismGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AMO.autismGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutismGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
